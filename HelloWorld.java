f5c7ab1ba4ada198503a3d2c10c582876e78c419
narmineba <narmineba@gmail.com>
2020-09-11 21:03:23 -0500

    more inclusive message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 class HelloWorldApp {
  public static void main(String[] args) {
- System.out.println("Hello World!"); // Display the string.
- System.out.println(“Says George.”);
+ System.out.println("Hello universe!"); // Display the string.
+ System.out.println(“Says Earth.”);
  }
 }
